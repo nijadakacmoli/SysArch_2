e1ab28a739d4fe64725f58cdf7646cf285152402
nijada.kacmoli <nika00012@stud.uni-saarland.de>
2024-07-14 00:34:33 +0200

    Exercise 2-1

--- programs/public/os/polling.s
+++ programs/public/os/polling.s
@@ -1,5 +1,25 @@
-# TODO: wait for keyboard input
-# TODO: read keyboard input
-# TODO: wait for display ready
-# TODO: print keyboard input to display
-# TODO: start again
+_start:
+    # Main loop
+loop:
+    # Wait for keyboard input
+    li t1, 0xffff0010
+keyboard_wait_loop:
+    lw t2, 0(t1)
+    beqz t2, keyboard_wait_loop  # If keyboard_ready is 0, loop
+
+    # Read keyboard input
+    li t1, 0xffff0014
+    lw a0, 0(t1)  # Load the ASCII value of the pressed key into a0
+
+    # Wait for display to be ready
+    li t1, 0xffff0018
+display_wait_loop:
+    lw t2, 0(t1)
+    beqz t2, display_wait_loop  # If display_ready is 0, loop
+
+    # Print keyboard input to display
+    li t1, 0xffff001c
+    sw a0, 0(t1)  # Write the ASCII value to display_data
+
+    # Start again
+    j loop
