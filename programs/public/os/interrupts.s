fe812615e13f6bc5ffc687461606be58829da3c2
nijada.kacmoli <nika00012@stud.uni-saarland.de>
2024-07-13 17:44:30 +0200

    Create interrupts.s

---
+++ programs/public/os/interrupts.s
@@ -0,0 +1,12 @@
+# TODO: set up exception handler
+# TODO: set up mepc to point to the first instruction of the fibonacci function
+# TODO: enable interrupts as needed
+# TODO: initialize data structure to buffer keyboard inputs
+# TODO: execute the fibonacci function until you get an interrupt
+
+exception_handler:
+    # TODO: save registers
+    # TODO: check if the cause of the exception is an interrupt
+    # TODO: check which device is ready and handle the interrupt
+    # TODO: after handling the interrupt it may be useful to check if keyboard or display are still or again ready
+    # TODO: restore registers you saved and return to user mode (to continue fibonacci computation) 
